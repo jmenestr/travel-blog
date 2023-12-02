bad62cd27a25fad22670a44579013f7f28d0187d
Justin Menestrina <jmenestr@gmail.com>
2023-12-02 09:47:20 -0700

    fix url

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://jmenestrina.github.io',
-  base: '/travel-blog/',
+  site: 'https://www.alliandjustin.com',
+  base: '/',
   integrations: [tailwind()]
 });
